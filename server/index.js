8a5daf47b06ee191b6af4bfdbe7bf9f2134621eb
Mohit Yadav <my6644866448@gmail.com>
2026-07-16 12:19:38 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatify-chat-application-client.onrender.com",
     credentials:true
 }))
 app.use(express.json())
